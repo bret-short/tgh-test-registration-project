3ac6e44d3339ca1d32b69697da9baa7e24c577a4
Bret Short <bretstephenshort@live.com>
2024-03-05 17:49:57 -0700

    remove an unused import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { UsersComponent } from './users/users.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 const routes: Routes = [
